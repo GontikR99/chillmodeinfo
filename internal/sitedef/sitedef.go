ff6c870f5f11662002372178fdce70421065a4ac
GontikR99 <nobody@nobody.com>
2021-03-05 13:51:37 -0500

    Updated to be slightly more responsive on small screens.

--- internal/sitedef/sitedef.go
+++ internal/sitedef/sitedef.go
@@ -12,4 +12,4 @@ var SiteURL = "https://" + DNSName + ":" + strconv.Itoa(Port)
 
 const GoogleSigninClientId = "465672423976-qn8u1junpmdanfnlbc2ne3dobfmf4nvp.apps.googleusercontent.com"
 
-const InactiveDuration = -14 * 24 * time.Hour
+const InactiveDuration = -7 * 24 * time.Hour
